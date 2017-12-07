529258f579ec349a671a29a2ea342d10ae136e87
MARINA ALVES QUEIROZ MALUTA <01547014679@jpmorgan.sp>
2017-12-07 21:59:11 -0200

    Subindo componente anuncio2

---
+++ src/components/Anuncio2/Anuncio2.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import "./CardProduct.css";
+
+export default (props) => (
+
+        <div className = "anuncio2">
+            <img src="./anuncio2.png" alt="Anuncio 2">
+        </div>
+
+);
