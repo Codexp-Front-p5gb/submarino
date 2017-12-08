ba203083ca9abbaf4a07633634273a18e1187d48
Flavia Guterres <guterres@gmail.com>
2017-12-08 14:47:50 -0200

    navbar html and css (not finished)

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+	<head>
+		<meta charset="UTF-8">
+		<title>Submarino</title>
+		<link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
+		<link rel="stylesheet" type="text/css" href="Navbar.css">
+	</head>
+
+	<body>
+        <div class="navbar">
+            <ul class="navbar_list">
+                <li class="navbar_list_item highlight"><a href="">Oferta Wow!</a></li>
+                <li class="navbar_list_item"><a href="">Cartão Sub</a></li>
+                <li class="navbar_list_item"><a href="">Frete Grátis no app</a></li>
+                <li class="navbar_list_item"><a href="">iPhone X</a></li>
+                <li class="navbar_list_item"><a href="">Prime</a></li>
+                <li class="navbar_list_item"><a href="">Venda no Sub</a></li>
+                <li class="navbar_list_item"><a href="">Para sua Empresa</a></li>
+                <li class="navbar_list_item"><a href="">Viagens</a></li>
+            </ul>
+        </div>
+    </body>
+</html>
